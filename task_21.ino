ead50758ebc6b0bd13bdd8c354f32cf119cc6ea0
jalotasa <62582433+jalotasa@users.noreply.github.com>
2021-08-23 22:59:47 +1000

    Add files via upload

---
+++ task_21.ino
@@ -0,0 +1,35 @@
+#define ledR 8
+#define ledG 7
+#define ledY 12
+int interrCount = 0;
+ 
+void setup() 
+{
+ 
+  pinMode( ledY,OUTPUT );
+  Serial.begin(9600);
+   
+
+  digitalWrite( ledY,0 );
+   
+  attachInterrupt(0, interruptChange, RISING );
+}
+ 
+void loop() {
+  interrCount++;
+  if ( interrCount == 10 ) 
+  {
+    interrCount = 0;
+    digitalWrite( ledY,0 );
+  }
+   
+  delay(300);
+
+}
+ 
+void interruptChange() 
+{
+  digitalWrite( ledY,1 );
+  Serial.print("High");
+  
+}
